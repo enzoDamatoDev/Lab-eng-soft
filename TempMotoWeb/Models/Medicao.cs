4a035cff491ea3fc1cf293a9e0ed0d86f9f4ca8c
enzoDamatoDev <enzodamato@outlook.com>
2023-09-25 17:13:37 -0300

    criar web site

---
+++ TempMotoWeb/Models/Medicao.cs
@@ -0,0 +1,15 @@
+﻿namespace TempMotoWeb.Models
+{
+    public class Medicao
+    {
+        public int Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public float Altitude { get; set; }
+        public float Temperatura { get; set; }
+        public float Umidade { get; set; }
+        public int Num_Satelites { get; set; }
+        public float Velocidade { get; set; }
+        public DateTime Data_Medicao { get; set; }
+    }
+}
---
+++ TempMotoWeb/obj/Release/net7.0/PubTmp/EFSQLScripts/TempMotoWeb.Data.TempMotoWebContext.sql
@@ -0,0 +1,40 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230925195734_FirstMigration')
+BEGIN
+    CREATE TABLE [Medicao] (
+        [Id] int NOT NULL IDENTITY,
+        [Latitude] float NOT NULL,
+        [Longitude] float NOT NULL,
+        [Altitude] real NOT NULL,
+        [Temperatura] real NOT NULL,
+        [Umidade] real NOT NULL,
+        [Num_Satelites] int NOT NULL,
+        [Velocidade] real NOT NULL,
+        [Data_Medicao] datetime2 NOT NULL,
+        CONSTRAINT [PK_Medicao] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230925195734_FirstMigration')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230925195734_FirstMigration', N'7.0.11');
+END;
+GO
+
+COMMIT;
+GO
+
