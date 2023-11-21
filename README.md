22d45d4ab84c46cded043968e6d3d914626ffcb8
enzoDamatoDev <enzodamato@outlook.com>
2023-11-20 22:20:50 -0300

    melhorar UI

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ repositorio da matéria Laboratorio de engenharia de software
 
 link do site
 https://tempmotoweb.azurewebsites.net/
+
+o site faz uso da assinatura de estudante do azure, que recebe créditos limitados para serem usados, a previsão é que esses créditos acabem em 27/11, porém podem acabar antes, se o site estiver fora do ar, este provavelmente é o motivo
--- TempMotoWeb/wwwroot/js/grafico.js
+++ TempMotoWeb/wwwroot/js/grafico.js
@@ -6,6 +6,27 @@ const myChart = new Chart(ctx, {
         datasets: [{
             label: [],
             data: [],
+            borderColor: '#FF6384',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const ctxu = document.getElementById('graficoUmidade');
+const graficoUmidade = new Chart(ctxu, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: [],
+            data: [],
             borderWidth: 1
         }]
     },
@@ -21,16 +42,26 @@ const myChart = new Chart(ctx, {
 async function montarGrafico() {
 
     myChart.clear();
-    myChart.data.labels = []
+    graficoUmidade.clear();
+
+    myChart.data.labels = [];
     myChart.data.datasets[0].data = [];
     myChart.data.datasets[0].label = 'temperatura';
 
+    graficoUmidade.data.labels = [];
+    graficoUmidade.data.datasets[0].data = [];
+    graficoUmidade.data.datasets[0].label = 'umidade';
+
     pontosGrafico.forEach(function (item, i) {
 
-        myChart.data.labels[i] = item.data_Medicao
+        myChart.data.labels[i] = item.data_Medicao;
         myChart.data.datasets[0].data[i] = item.temperatura;
+
+        graficoUmidade.data.labels[i] = item.data_Medicao;
+        graficoUmidade.data.datasets[0].data[i] = item.umidade;
 
     });
 
-    myChart.update()
+    myChart.update();
+    graficoUmidade.update();
 }
